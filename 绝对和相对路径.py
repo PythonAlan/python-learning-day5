c10359e3269a1f81a29467b6d2ffc4c249ed0f5c
Alan <371725153@qq.com>
2016-02-26 00:00:03 +0800

    绝对和相对路径

---
+++ 绝对和相对路径.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#antuor:Alan
+
+
+import os
+path = os.getcwd()
+print(path)
+'''/Users/Alan/Desktop/AlanProject/day5'''
+path_a = os.path.dirname(os.path.abspath(__file__))
+print (path_a)
+'''/Users/Alan/Desktop/AlanProject/day5'''
+print(__file__)
+'''绝对和相对路径.py'''
